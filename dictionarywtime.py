336f8b5c49c15d90cb62a8f0b7aefa700ec46a7a
Antonella <coccosc@hotmail.com>
2021-05-13 12:24:49 +0400

    Add files via upload

---
+++ dictionarywtime.py
@@ -0,0 +1,15 @@
+#Time words DICTIONARY
+months = {
+	1 : "January",
+	2 : "February",
+	3 : "March",
+	4 : "April",
+	5 : "May",
+	6 : "June",
+	7 : "July",
+	8 : "Ausgust",
+	9 : "September",
+    10 : "October",
+    11 : "November",
+    12 : "December"
+	}
